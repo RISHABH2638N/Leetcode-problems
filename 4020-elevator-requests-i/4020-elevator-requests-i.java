8c9c4df74a3cb7585f2e092cc2309d34fbe08cfd
RISHABH2638N <rishankmishra5@gmail.com>
2026-08-18 03:35:52 +0530

    Time: 1 ms (98.39%), Space: 44.8 MB (44.34%) - LeetHub

--- 4020-elevator-requests-i/4020-elevator-requests-i.java
+++ 4020-elevator-requests-i/4020-elevator-requests-i.java
@@ -2,7 +2,8 @@ class Solution {
     public int elevatorRequests(int n, int[] requests) {
         int t=0;
         int F=0;
-        for(int i=0; i<requests.length; i++){
+        n=requests.length;
+        for(int i=0; i<n; i++){
             t=t+Math.abs(requests[i]-F);
             F=requests[i];
         }
